3b2fbd3be534f3a228a433e3db931c663e55df74
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-12-29 19:42:51 +0530

    Create 2. Datatypes.java

---
+++ Lecture-Code/2. Datatypes.java
@@ -0,0 +1,16 @@
+import java.util.PrimitiveIterator;
+
+public class Practice1{
+    public static void main(String[] args) {
+        // Primitive Datatypes
+        int rollno = 54;
+        char letter = 's';
+        float marks = 95.58f;
+        double large_decimal_integer = 645684684.6488168168;
+        long large_integer = 626523585463125L;
+        boolean check = false;
+
+        // Non-Primitive Datatypes
+        String name = "hello";
+    }
+}
