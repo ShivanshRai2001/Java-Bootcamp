bb9d43b1b02d76a9587e50a3828db0355cef4479
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-12-29 22:06:41 +0530

    Create 3. Scanner Class.java

---
+++ Lecture-Code/3. Scanner Class.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Practice1{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = input.nextInt();
+        System.out.println("Entered number is : " + num);
+    }
+}
