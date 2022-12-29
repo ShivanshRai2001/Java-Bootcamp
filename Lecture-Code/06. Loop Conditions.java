1cc35642e3cca726cf5b39534f572b033d267ec2
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2022-12-29 23:21:50 +0530

    Create 06. Loop Conditions.java

---
+++ Lecture-Code/06. Loop Conditions.java
@@ -0,0 +1,21 @@
+public class Practice1{
+    public static void main(String[] args) {
+        // if conditions
+        int a = 10;
+        if (a == 10){
+            System.out.println("Hello.");
+        }
+
+        // while loop
+        int count = 1;
+        while (count != 5){
+            System.out.println(count);
+            count++;
+        }
+
+        // for loop
+        for(int count1=1; count1!=5; count1++){
+            System.out.println(count1);
+        }
+    }
+}
