d0f454beac6134da0b948055b0fa7c24a92a01e3
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-03 22:44:07 +0530

    Update 10. Greatest Number.java

--- Assignment/10. Greatest Number.java
+++ Assignment/10. Greatest Number.java
@@ -18,3 +18,38 @@ public class Practice1 {
         }
     }
 }
+
+// When the input is of the three number
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int num1 = input.nextInt();
+
+        System.out.print("Enter the second number : ");
+        int num2 = input.nextInt();
+
+        System.out.print("Enter the third number : ");
+        int num3 = input.nextInt();
+
+        int max = num1;
+        if(num2 > max) {
+            max = num2;
+        }
+        if(num3 > max){
+            max = num3;
+        }
+
+        System.out.println("The greatest number is : " + max);
+
+        // if(num1 > num2){
+        //     System.out.println("The greatest number is : " + num1);
+        // }
+        // else {
+        //     System.out.println("The greatest number is : " + num2);
+        // }
+    }
+}
