7081dbffadb7cf9a22edfdfc2d65b29bcd897720
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-06 21:29:15 +0530

    Update 18. Input and sum till special character.java

--- Assignment/18. Input and sum till special character.java
+++ Assignment/18. Input and sum till special character.java
@@ -1,3 +1,27 @@
+// Sum Of N Numbers
+
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the natural number : ");
+        int ele = input.nextInt();
+
+        int sum = 0;
+
+        for(int i=1; i<=ele; i++) {
+            System.out.print("Enter the " + i + " numbers : ");
+            int num = input.nextInt();
+
+            sum += num;
+        }
+        System.out.println("The sum of the number is : " + sum);
+    }
+}
+
+
 // Take integer inputs till the user enters 0 and print the sum of all numbers 
 
 import java.util.Scanner;
