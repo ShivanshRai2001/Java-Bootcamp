b362a2e69e1719e6e22e1bf8c407c57b9043fe40
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-11 22:46:27 +0530

    Update 14. Sum using Functions.java

--- Lecture-Code/14. Sum using Functions.java
+++ Lecture-Code/14. Sum using Functions.java
@@ -16,3 +16,27 @@ public class Practice2 {
         System.out.println("The sum of the number is : " + sum);
     }
 }
+
+
+// It can also be written as the following way : 
+import java.util.Scanner;
+
+public class Practice2 {
+    // Return Value
+    public static void main(String[] args) {
+        int ans = sum();      // Funcation call
+        System.out.println("The sum of the number is : " + ans);
+    }
+
+    static int sum() {     // Funcation declaration
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        int num1 = input.nextInt();
+        System.out.print("Enter the second number : ");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        // System.out.println("The sum of the number is : " + sum);
+        return sum;
+    }
+}
