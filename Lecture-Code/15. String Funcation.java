df7316b6ace911d11b2df9a7b326cc351e45bb29
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-12 12:10:35 +0530

    Create 15. String Funcation.java

---
+++ Lecture-Code/15. String Funcation.java
@@ -0,0 +1,12 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+
+    static String greet() {
+        String greeting = "Hello!";
+        return greeting;
+    }
+}
+
