5b8fc4d82157fb9509dea739835c5a37c09ccad5
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-12 21:56:45 +0530

    Update 17. Varargs.java

--- Lecture-Code/17. Varargs.java
+++ Lecture-Code/17. Varargs.java
@@ -9,3 +9,17 @@ public class Practice2 {
         System.out.println(Arrays.toString(v));
     }
 }
+
+
+// For multiple variable
+import java.util.Arrays;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        multiple(2, 3, "Kunal", "Rahul");
+    }
+
+    static void multiple(int a, int b, String ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+}
