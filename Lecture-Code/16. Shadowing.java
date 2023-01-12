50d5b08e2016f4ef58238f0f59c8c5ff6442bb30
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-12 21:21:20 +0530

    Update 16. Shadowing.java

--- Lecture-Code/16. Shadowing.java
+++ Lecture-Code/16. Shadowing.java
@@ -1,8 +1,15 @@
 public class Practice2 {
-    static int x = 90;  // this will be shadowed at line 8
+    static int x = 90;  // this will be shadowed at line 6
     public static void main(String[] args) {
         System.out.println(x);  // 90
-        int x = 20;
+        int x = 20;     // the class variable at line 2 is shadowed by this
+
+        /*
+        int x;
+        System.out.println(x);  // scope will begin when value is initialised
+        x = 20;
+        */
+
         System.out.println(x);  // 20
         fun();
     }
