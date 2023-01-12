8d2ca57697a7fe46389c44f35d4434442861e8b4
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-12 22:05:36 +0530

    Create 18. Funcation Overloading.java

---
+++ Lecture-Code/18. Funcation Overloading.java
@@ -0,0 +1,14 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        fun(45);
+        fun("ABC");
+    }
+
+    static void fun(int a) {
+        System.out.println(a);
+    }
+
+    static void fun(String name) {
+        System.out.println(name);
+    }
+}
