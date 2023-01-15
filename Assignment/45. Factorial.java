3a28fe96c0a0eb8f50ab906b910fbd338a3e28ad
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-15 13:16:03 +0530

    Create 45. Factorial.java

---
+++ Assignment/45. Factorial.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = input.nextInt();
+        int fac = 1;
+        int ans = factorial(fac, num);
+        System.out.println(ans);
+    }
+
+    static int factorial(int fac, int num) {
+        if(num == 1 || num == 0) {
+            System.out.println("1");
+        }
+
+        for(int i=1; i<=num; i++) {
+            fac = fac * i;
+        }
+        return fac;
+    }
+}
