095dda057dc7b38a26625f0e49075564037520ce
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-17 22:58:22 +0530

    Update 55. Basic Calculator Func.java

--- Assignment/55. Basic Calculator Func.java
+++ Assignment/55. Basic Calculator Func.java
@@ -49,3 +49,43 @@ public class Practice2 {
         // return div;
     }
 }
+
+
+// Another way to write is
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first number : ");
+        float num1 = input.nextFloat();
+        System.out.print("Enter the second number : ");
+        float num2 = input.nextFloat();
+        System.out.print("Enter the operation : ");
+        char op = input.next().charAt(0);
+
+        calc(num1, num2, op);
+    }
+
+    static void calc(float num1, float num2, char op) {
+        if(op == '+') {
+            float sum = num1 + num2;
+            System.out.println("The sum of the number is : " + sum);
+        }
+
+        else if(op == '-') {
+            float sub = num1 - num2;
+            System.out.println("The subtraction of number is : " + sub);
+        }
+
+        else if(op == '*') {
+            float mul = num1 * num2;
+            System.out.println("The multiplication of number is : " + mul);
+        }
+        else if(op == '/') {
+            float div = num1 / num2;
+            System.out.println("The division of number is : " + div);
+        }
+    }
+}
