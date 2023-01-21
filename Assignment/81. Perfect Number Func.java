8a3f8ddc82af45f0901e4b79df26ca5f63924661
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-21 09:18:32 +0530

    Create 81. Perfect Number Func.java

---
+++ Assignment/81. Perfect Number Func.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = input.nextInt();
+
+        System.out.println("The sum of the factorial of number is : " + perfect(num));
+    }
+
+    static int perfect(int num) {
+        int sum = 0;
+        for(int i=1; i<num; i++) {
+            if(num%i == 0) {
+                sum += i;
+            }
+        }
+
+        if(sum == num) {
+            System.out.println("Perfect number.");
+        }
+        else {
+            System.out.println("Not a perfect number.");
+        }
+        return sum;
+    }
+}
