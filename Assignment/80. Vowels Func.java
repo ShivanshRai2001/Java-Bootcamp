fce3a5a909462744d90a98a15556dac694a8453b
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-01-21 09:12:47 +0530

    Create 80. Vowels Func.java

---
+++ Assignment/80. Vowels Func.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the string : ");
+        String str = input.nextLine();
+
+        vol(str);
+    }
+
+
+    static void vol(String str) {
+        int len = str.length();
+        String vowel = "";
+        String conso = "";
+
+        for(int i=0; i<len; i++) {
+            char vol = str.charAt(i);
+            if(vol == 'a' || vol == 'e' || vol == 'i' || vol == 'o' || vol == 'n' || vol == 'A' || vol == 'E' || vol == 'I' || vol == 'O' || vol == 'N') {
+                vowel += vol;
+            }
+            else {
+                conso += vol;
+            }
+        }
+        System.out.println("The vowels are : " + vowel);
+        System.out.println("The consonents are : " + conso);
+    }
+}
