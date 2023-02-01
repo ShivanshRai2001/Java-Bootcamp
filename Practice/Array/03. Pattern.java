a7ce644beb401222c5051b0500ceb9ed6e78ce02
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-01 23:12:26 +0530

    Create 03. Pattern.java

---
+++ Practice/Array/03. Pattern.java
@@ -0,0 +1,12 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        int[][] arr = new int[10][10];
+
+        for(int i=0; i<10; i++) {
+            for(int j=0; j<10; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println(" * ");
+        }
+    }
+}
