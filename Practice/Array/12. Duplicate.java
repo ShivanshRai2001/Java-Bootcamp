a4f6f234c193546c7b43a30a9d57570f695c4d3f
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-06 19:31:55 +0530

    Update 12. Duplicate.java

--- Practice/Array/12. Duplicate.java
+++ Practice/Array/12. Duplicate.java
@@ -1,6 +1,23 @@
+// Integer Value
 public class Practice2 {
     public static void main(String[] args) {
         int[] arr = {2, 5, 8, 7, 5};
+        
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=0; j<arr.length; j++) {
+                if((arr[i] == arr[j]) && (i != j)) {
+                    System.out.println("Duplicate value is : " + arr[j]);
+                }
+            }
+        }
+    }
+}
+
+
+// String Values
+public class Practice2 {
+    public static void main(String[] args) {
+        String[] arr = {"abc", "def", "abc", "fds", "hry"};
         
         for(int i=0; i<arr.length-1; i++) {
             for(int j=0; j<arr.length; j++) {
