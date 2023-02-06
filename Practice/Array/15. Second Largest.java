5943aba2e4fde27b5deedeef34f9e3d81b39b9f5
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-06 23:09:52 +0530

    Create 15. Second Largest.java

---
+++ Practice/Array/15. Second Largest.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 7, 6, 8};
+
+        System.out.println("Original Array : " + Arrays.toString(arr));
+
+        Arrays.sort(arr);
+
+        System.out.println("The second largest number is : " + Arrays.toString(arr));
+
+        for(int i=0; i<arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
+        }
+
+        System.out.println("The reverse array is : " + Arrays.toString(arr));
+        System.out.println("The second largest element is : " + arr[1]);
+    }
+}
