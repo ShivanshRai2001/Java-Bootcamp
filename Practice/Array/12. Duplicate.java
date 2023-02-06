8f888fc7d56ce0bceb1d94c54037b7614db8b71b
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-06 19:25:41 +0530

    Create 12. Duplicate.java

---
+++ Practice/Array/12. Duplicate.java
@@ -0,0 +1,13 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 7, 5};
+        
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=0; j<arr.length; j++) {
+                if((arr[i] == arr[j]) && (i != j)) {
+                    System.out.println("Duplicate value is : " + arr[j]);
+                }
+            }
+        }
+    }
+}
