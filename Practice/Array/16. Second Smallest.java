5c0b88a2b7e90b9b17710169035f1e8cf90a8353
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-06 23:17:32 +0530

    Update 16. Second Smallest.java

--- Practice/Array/16. Second Smallest.java
+++ Practice/Array/16. Second Smallest.java
@@ -11,3 +11,33 @@ public class Practice2 {
         System.out.println("The second smallest element is : " + arr[1]);
     }
 }
+
+
+// Long Method
+import java.util.Arrays;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 7, 6, 8, 3};
+
+        System.out.println("Original numeric array : "+Arrays.toString(arr));
+
+        int min = Integer.MAX_VALUE;
+        int second_min = Integer.MAX_VALUE;
+
+        for (int i=0; i<arr.length; i++) {
+            if(arr[i]==min) {
+                second_min=min;
+            } 
+            else if (arr[i] < min) {
+                second_min = min;
+                min = arr[i];
+            } 
+            else if (arr[i] < second_min) {
+                second_min = arr[i];
+            }
+        }
+
+        System.out.println("Second lowest number is : " + second_min);
+    }
+}
