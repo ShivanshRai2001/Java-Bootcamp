c5c2eaa86d18653364792e9535765bea5741edb4
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-07 12:59:53 +0530

    Update 17. Matrices Addition.java

--- Practice/Array/17. Matrices Addition.java
+++ Practice/Array/17. Matrices Addition.java
@@ -19,3 +19,53 @@ public class Practice2 {
         }
     }
 }
+
+
+// Long Method
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input number of rows : ");
+        int row = input.nextInt();
+        System.out.println("Input number of columns of matrix");
+        int col  = input.nextInt();
+ 
+        int arr1[][] = new int[row][col];
+        int arr2[][] = new int[row][col];
+        int sum[][] = new int[row][col];
+        
+        System.out.println("Input elements of first matrix");
+        
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                arr1[i][j] = input.nextInt();
+            }
+        }
+        
+        System.out.println("Input the elements of second matrix");
+        
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                arr2[i][j] = input.nextInt();
+            }
+        }
+
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                sum[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        System.out.println("Sum of the matrices:-");
+        
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                System.out.print(sum[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
