256ca4655a74e8fa81bf0c120c2261f36aabb6f3
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-07 13:05:30 +0530

    Create 18. Array to ArrayList.java

---
+++ Practice/Array/18. Array to ArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        String[] arr = new String[] {"Python", "JAVA", "PHP",  "Perl", "C#", "C++"};
+        System.out.println(Arrays.toString(arr));
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(arr));
+        System.out.println(list);
+
+        list.add("HTML");
+        System.out.println(list);
+    }
+}
