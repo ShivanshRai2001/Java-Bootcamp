60c4c126b712b058932f9227a3237d0a88096053
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-08 23:32:25 +0530

    Create 28. Eliminate Duplicate and count.java

---
+++ Practice/Array/28. Eliminate Duplicate and count.java
@@ -0,0 +1,15 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        int[] arr = {20, 20, 30, 40, 50, 50, 50};
+        System.out.println("Original length : " + arr.length);
+
+        int index = 0;
+        for(int i=1; i<arr.length; i++) {
+            if(arr[i] != arr[i-1]) {
+                arr[index++] = arr[i];
+            }
+        }
+
+        System.out.println("After duplication removal : " + index);
+    }
+}
