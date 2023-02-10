8607777880dd8d2295bcd78497648cc91bd0bd63
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-10 15:42:05 +0530

    Update 30. Find Index of element.java

--- Practice/Array/30. Find Index of element.java
+++ Practice/Array/30. Find Index of element.java
@@ -16,3 +16,30 @@ public class Practice4 {
         System.out.println("The index of the number is : " + index);
     }
 }
+
+
+// Using for loop
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practice4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int index = 0;
+        int[] arr = {1, 5, 8, 14, 20, 23};
+        System.out.println("The array is : " + Arrays.toString(arr));
+
+        System.out.println("Enter the number of find its array : ");
+        int num = input.nextInt();
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == num) {
+                index = i;
+                break;
+            }
+        }
+
+        System.out.println("The index of the number is : " + index);
+    }
+}
