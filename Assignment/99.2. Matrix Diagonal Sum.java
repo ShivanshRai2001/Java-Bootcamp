cc9d6e04714957277b5616c417e1e8b924c269ab
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-12 17:34:42 +0530

    Update 99.2. Matrix Diagonal Sum.java

--- Assignment/99.2. Matrix Diagonal Sum.java
+++ Assignment/99.2. Matrix Diagonal Sum.java
@@ -28,3 +28,31 @@ public class Practice2 {
         System.out.println(total);
     }
 }
+
+
+ // Short Method
+public class Practice2 {
+    public static void main(String[] args) {
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        // int[][] matrix = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 1}, {2, 3, 4, 5, 6}, {7, 8, 9, 1, 2}, {3, 4, 5, 6, 7}};
+        int sum = 0;
+        int rem = 0;
+        int total = 0;
+
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=0; j<matrix.length; j++) {
+                if(i == j) {
+                    sum += matrix[i][j];
+                }
+                
+                if(i+j == matrix.length-1) {
+                    sum += matrix[i][j];
+                }
+            }
+        }
+
+        rem = (matrix.length - 1)/2;
+        total = sum - matrix[rem][rem];
+        System.out.println(total);
+    }
+}
