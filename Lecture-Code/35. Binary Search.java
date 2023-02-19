65df3d2797a6dbcaa828bbb4cc1344e99470a9f4
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-19 16:47:03 +0530

    Update 35. Binary Search.java

--- Lecture-Code/35. Binary Search.java
+++ Lecture-Code/35. Binary Search.java
@@ -1,6 +1,7 @@
 public class Practice2 {
     public static void main(String[] args) {
         int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        // int[] arr = {89, 45, 22, 18, 16, 15, 4, 3, 2, 0, -4, -12, -18};
         int target = 22;
         System.out.println(binarySearch(arr, target));
     }
@@ -11,13 +12,13 @@ public class Practice2 {
 
         while(start <= end) {
             // int mid = (start + end) / 2;
-            int mid = start + (end - start) / 2;
+            int mid = start + (end - start) / 2; // better way of doing this as it does not exceds the limit
 
-            if(target < arr[mid]) {
+            if(target < arr[mid]) { // For decending order the operation will change to >
                 end = mid - 1;
             }
 
-            else if(target > arr[mid]) {
+            else if(target > arr[mid]) { // For decending order the operation will change to <
                 start = mid + 1;
             }
 
