76b09992a513aa573d8628f646af921fba3686ea
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-22 22:07:12 +0530

    Create Right Angle Number Triangle 6.java

---
+++ Practice/Pattern/Right Angle Number Triangle 6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = input.nextInt();
+
+        for(int i=0; i<num; i++) {
+            for(int x = num-i; x>0; x--) {
+                System.out.print(" ");
+            }
+            for(int y=0; y<=i; y++) {
+                System.out.print("@");
+            }
+            System.out.println();
+        }
+    }
+}
