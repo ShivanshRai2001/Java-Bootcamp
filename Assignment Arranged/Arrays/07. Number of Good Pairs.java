d9611be9d41279a77aa909702ba0083e4efbe273
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-02-23 21:37:52 +0530

    Create 07. Number of Good Pairs.java

---
+++ Assignment Arranged/Arrays/07. Number of Good Pairs.java
@@ -0,0 +1,16 @@
+public class Practice2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1, 1, 3};
+        int count = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            for(int j=i+1; j<arr.length; j++) {
+                if(arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+    }
+}
