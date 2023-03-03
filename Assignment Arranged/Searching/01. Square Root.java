4f40cae8f7f098035bf06a68fbc9cdc30436a80f
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-03-03 22:56:55 +0530

    Update 01. Square Root.java

--- Assignment Arranged/Searching/01. Square Root.java
+++ Assignment Arranged/Searching/01. Square Root.java
@@ -4,3 +4,36 @@ public class Practice2 {
         System.out.println(ans);
     }
 }
+
+
+// Using Binary Search
+public class Practice2 {
+    public static void main(String[] args) {
+        int num = 8;
+
+        System.out.println(sqrt(num));
+    }
+
+    static int sqrt(int num) {
+        if(num == 0) {
+            return 0;
+        }
+
+        int first = 1;
+        int last = num;
+
+        while(first <= last) {
+            int mid = first + (last - first) / 2;
+            if(mid == num / mid) {
+                return mid;
+            }
+            else if(mid > num / mid) {
+                last = mid - 1;
+            }
+            else {
+                first = mid + 1;
+            }
+        }
+        return last;
+    }
+}
