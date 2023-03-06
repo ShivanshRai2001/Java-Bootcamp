0ab5a3db9c4a0247404efda650d4f184d837a71e
ShivanshRai2001 <110758483+ShivanshRai2001@users.noreply.github.com>
2023-03-06 22:49:41 +0530

    Update 05. Valid Perfect Square.java

--- Assignment Arranged/06. Searching/05. Valid Perfect Square.java
+++ Assignment Arranged/06. Searching/05. Valid Perfect Square.java
@@ -19,3 +19,38 @@ public class Practice2 {
         System.out.println(ans);
     }
 }
+
+
+// using binary search
+import java.util.Scanner;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the number : ");
+        int num = input.nextInt();
+
+        System.out.println(square(num));
+    }
+
+    static boolean square(int num) {
+        int start = 1;
+        int end = num;
+
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+
+            if(mid*mid == num) {
+                return true;
+            }
+            else if(mid*mid < num) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
